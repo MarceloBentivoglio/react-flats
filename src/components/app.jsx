8eff2a735290f771218930eb67d7fc29a1031db6
Marcelo Bentivoglio <mabj90s@gmail.com>
2020-11-26 19:02:47 -0300

    added google maps, corrected some logic in the react flat and flat list part (didnt get it tho)

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 import FlatList from './flat_list.jsx';
 import flats from '../../data/flats.js';
+import Marker from './marker';
+
 
 class App extends Component {
 
@@ -9,16 +12,36 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			flats: flats,
-		}
+			flats,
+			selectedFlat: flats[0]
+		};
 	}
 
+	center() {
+		return {
+		  lat: this.state.selectedFlat.lat,
+		  lng: this.state.selectedFlat.lng
+		};
+	}
+
+	selectFlat = (index) => {
+		this.setState({ selectedFlat: flats[index] });
+	}
 
 
 	render () {
 		return (
 			<div>
-				<FlatList flats={this.state.flats}/>
+				<FlatList 
+					flats={this.state.flats}
+					selectedFlat={this.state.selectedFlat}
+					selectFlat={this.selectFlat}
+				/>
+				<div className="map-container">
+		          <GoogleMapReact defaultCenter={this.center()} defaultZoom={10}>
+		            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+		          </GoogleMapReact>
+		        </div>
 			</div>
 		)
 	}
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-
-import Flat from './flat.jsx';
+import React from 'react';
+import Flat from './flat';
 
 const FlatList = (props) => {
+  const renderList = () => {
+    return props.flats.map((flat, index) => {
+      return (
+        <Flat
+          flat={flat}
+          key={flat.lat}
+          selected={flat.name === props.selectedFlat.name}
+          index={index}
+          selectFlat={props.selectFlat}
+        />
+      );
+    });
+  };
 
-		return (
-			<div className="flat-list">
-				{props.flats.map(flat => 
-					<Flat 
-					  flat={flat.flat} 
-					  key={flat.lat}
-					  imageUrl={flat.imageUrl} 
-					/>
-				)}
-			</div>
-		);
+  return (
+    <div className="flat-list">
+      {renderList()}
+    </div>
+  );
 };
 
 export default FlatList;
